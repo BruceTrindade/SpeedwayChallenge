bfc883b28c1543dc6430d42eba0a85ba89feb473
brucetrindade <brucetrindade2000@gmail.com>
2022-03-05 18:38:40 -0300

    Challenge done

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+
 
 int main() {
   
   int c, n;
-  
-  cin >> c;
-  cin >> n;
-  
-  cout << "Ponto de finalizacao da corrida: " << (c%n);
+  bool continua = true;
+
+  while (continua)
+  {
+       cin >> c;
+       cin >> n;
+       if ((c <= 1 || c >= pow(10,8)) || ((n <= 1 || n >= 100)) )
+       {
+           cout << "Number not accepted, please try again" << endl;
+       } else {
+            continua = false;
+            cout << c%n;
+       }
+    
+  }
   
   return 0;
+
 }
